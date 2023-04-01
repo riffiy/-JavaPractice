41ef2d934c1813de18b51eabe4ddecdd8dd6adc5
riffi <riffi@LAPTOP-0QP0QAN4>
2023-04-01 12:58:32 +0900

    view page practice

---
+++ src/practice/SwingAppMain.java
@@ -0,0 +1,16 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class SwingAppMain {
+
+	public static void main(String[] args) {
+		JFrame mainFrame = new JFrame("SampleSwing");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(640, 480);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+
+	}
+
+}
